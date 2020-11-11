ae87cac3621ac563784665bbd21ee5ccff31a997
zhouhongtu <zhouhongtu@tongji.edu.cn>
2020-11-11 15:58:26 +0800

    remove old planner

--- src/modules/planner/visualization/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "visualization.h"
-using namespace std;
-
-int main(){
-	Visualization* vs = Visualization::getInstance();
-	vs->print_text<bool>("testKey", true);
-	vs->visualize();
-	return 0;
-}
