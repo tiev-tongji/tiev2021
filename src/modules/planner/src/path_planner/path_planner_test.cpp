910d3549c14677063fd659c85dded8d244c36901
MilesChing <qinzhengrui@live.cn>
2021-03-03 16:19:30 +0800

    new interactive path planner test

--- src/modules/planner/src/path_planner/path_planner_test.cpp
+++ src/modules/planner/src/path_planner/path_planner_test.cpp
@@ -1,156 +1,164 @@
-#include "path_planner.h"
-#include "config/config.h"
+#include "path_planner/path_planner.h"
+#include "planner_common/config.h"
+#include "planner_common/pose.h"
+#include "planner_common/const.h"
 #include <iostream>
-#include "nature.h"
 #include <termio.h>
 #include <thread>
-#include "nature/point.h"
 #include "opencv2/opencv.hpp"
 
 using namespace std;
 using namespace TiEV;
 using namespace cv;
 
-#ifdef NO_SPEED_PLANNER
+enum OperationStatus {
+    WaitingForTargetPoint,
+    WaitingForTargetAngle,
+    Planning,
+};
 
-int mmp[GRID_ROW][GRID_COL] = {0};
+OperationStatus operationStatus = WaitingForTargetPoint;
+TiEV::Pose      targetPose;
+char*           map_file_path = NULL;
+bool            backward_enabled = false;
+double          current_speed    = 0;
 
-vector<TiEV::Point> res[MAX_TARGET_NUM];
-
-int getch(void) {
-    int cr;
-    struct termios nts, ots;
-    if (tcgetattr(0, &ots) < 0) return EOF;
-    nts = ots;
-    cfmakeraw(&nts);
-    if (tcsetattr(0, TCSANOW, &nts) < 0) return EOF;
-    cr = getchar();
-    if (tcsetattr(0, TCSANOW, &ots) < 0) return EOF;
-    return cr;
+void usage() {
+    exit(0);
 }
 
-void setColor(cv::Mat& mat, int row, int col, u_char r, u_char g, u_char b){
-    mat.at<cv::Vec3b>(row, col)[0] = b;
-    mat.at<cv::Vec3b>(row, col)[1] = g;
-    mat.at<cv::Vec3b>(row, col)[2] = r;
-}
-
-int main(){
-    Config* config = Config::getInstance();
-    PathPlanner* planner = PathPlanner::getInstance();
-    PathPlannerView* plannerView = PathPlannerView::getInstance();
-
-    vector<TiEV::Point> targets;
-    planner->setStartMaintainedPath(vector<TiEV::Point>());
-    planner->setBackwardEnabled(true);
-
-    cv::namedWindow("PathPlanner Test", cv::WINDOW_KEEPRATIO);
-
-    double ang;
-    cout << "Target number: ";
-    int n;
-    while(cin >> n){
-        targets.resize(n);
-        for(int i = 0; i < n; ++i){
-            cout << "Set StartPoint(x,y,a): ";
-            cin >> targets[i].x >> targets[i].y >> ang;
-            targets[i].angle.setByRad(ang);
+void read_args(int argc, char** argv) {
+    for(int i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "-b") == 0 || strcmp(argv[i], "--enable-backward") == 0) {
+            cout << "backward enabled" << endl;
+            backward_enabled = true;
         }
+        else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--speed") == 0) {
+            if (i + 1 < argc) {
+                current_speed = atof(argv[i++]);
+                cout << "current speed set: " << current_speed << " m/s" << endl;
+            }
+            else {
+                cout << "error using flag " << argv[i] << " , argument missing." << endl;
+                usage();
+            }
+        }
+        else if (map_file_path == NULL) {
+            map_file_path = argv[i];
+            cout << "map file path: " << map_file_path << endl;
+        }
+        else {
+            cout << "unrecognized input " << argv[i] << endl;
+            usage();
+        }
+    }
+}
 
-        cv::Mat view = cv::Mat::zeros(401, 151, CV_8UC3);
-        cv::Mat cvmmp = cv::imread("/home/autolab/IV_planner/IV_path_exp/maps/map-09.png");
-        cv::resize(cvmmp, cvmmp, cv::Size(151, 401));
+void on_mouse(int event, int x, int y, int flags, void *ustc)
+{
+	switch (operationStatus) {
+        case WaitingForTargetPoint: {
+            if (event == CV_EVENT_LBUTTONDOWN) {
+                targetPose.x = y;
+                targetPose.y = x;
+                operationStatus = WaitingForTargetAngle;
+            }
+            break;
+        }
+        case WaitingForTargetAngle: {
+            if (event == CV_EVENT_LBUTTONUP) {
+                targetPose.ang = atan2(x - targetPose.y, y - targetPose.x);
+                operationStatus = Planning;
+            }
+            break;
+        }
+    }
+}
 
-		cout << "Set speed in km/h: ";
-		double spd;
-		cin >> spd;
-		spd /= 3.6;
+void generate_safe_map(double safe_map[MAX_ROW][MAX_COL]) {
+    queue<pair<int, int>> que;
+    for(int i = 0; i < MAX_ROW; ++i)
+        for(int j = 0; j < MAX_COL; ++j)
+            if(safe_map[i][j] == 0)
+                que.push(make_pair(i, j));
 
-        memset(mmp, 0x7f, sizeof(mmp));
-
-        queue<pair<int, int>> que;
-        for(int i = 0; i < 401; ++i)
-            for(int j = 0; j < 151; ++j)
-                if(cvmmp.at<cv::Vec3b>(i, j)[0] < 200){
-                    mmp[i][j] = 0;
-                    que.push(make_pair(i, j));
-                }
-
-        while(!que.empty()){
-            int nx = que.front().first;
-            int ny = que.front().second;
-            que.pop();
-            for(int dx = -1; dx <= 1; ++dx)
-                for(int dy = -1; dy <= 1; ++dy){
-                    int tx = dx + nx;
-                    int ty = dy + ny;
-                    if(tx >= 0 && tx < GRID_ROW &&
-                        ty >= 0 && ty < GRID_COL){
-                        if(mmp[nx][ny] + 1 < mmp[tx][ty]){
-                            mmp[tx][ty] = mmp[nx][ny] + 1;
-                            que.push(make_pair(tx, ty));
-                        }
+    while(!que.empty()){
+        int nx = que.front().first;
+        int ny = que.front().second;
+        que.pop();
+        for(int dx = -1; dx <= 1; ++dx){
+            for(int dy = -1; dy <= 1; ++dy){
+                int tx = dx + nx;
+                int ty = dy + ny;
+                if(tx >= 0 && tx < MAX_ROW &&
+                    ty >= 0 && ty < MAX_COL){
+                    if(safe_map[nx][ny] + 1 < safe_map[tx][ty]){
+                        safe_map[tx][ty] = safe_map[nx][ny] + 1;
+                        que.push(make_pair(tx, ty));
                     }
                 }
+            }
         }
+    }
+}
 
-        planner->setBackwardEnabled(false);
-        planner->setCurrentSpeed(spd);
-        planner->setStartMaintainedPath(vector<TiEV::Point>());
-        planner->setAbsSafeMap(mmp);
-        planner->setLaneSafeMap(mmp);
-        targets[0].angle.setByRad(ang);
-        planner->setTargets(targets);
-        time_t start_time = getTimeStamp();
+void draw(PathPlanner* planner, cv::Mat& view) {
+    // draw results
+    auto res = vector<SpeedPath>();
+    planner->getResults(res);
+    for(auto& path : res) {
+        for(auto& p : path.path) {
+            int i = round(p.x), j = round(p.y);
+            view.at<cv::Vec3b>(i, j) = cv::Vec3b(0x00, 0x00, 0xff);
+        }
+    }
+    // draw target pose
+    cv::arrowedLine(view,
+        cv::Point(targetPose.y, targetPose.x),
+        cv::Point(
+            targetPose.y + 20 * sin(targetPose.ang),
+            targetPose.x + 20 * cos(targetPose.ang)
+        ),
+        cv::Scalar(255, 0, 0)
+    );
+    // draw start pose
+    cv::arrowedLine(view,
+        cv::Point(CAR_CEN_COL, CAR_CEN_ROW),
+        cv::Point(CAR_CEN_COL, CAR_CEN_ROW - 20),
+        cv::Scalar(255, 128, 0)
+    );
+}
+
+int main(int argc, char** argv){
+    read_args(argc, argv);
+    PathPlanner* planner = PathPlanner::getInstance();
+    cv::namedWindow("PathPlanner Test", cv::WINDOW_NORMAL);
+    cv::setMouseCallback("PathPlanner Test", on_mouse);
+    cv::Mat map_image = cv::imread(map_file_path);
+    cv::resize(map_image, map_image, cv::Size(MAX_COL, MAX_ROW));
+    double safe_map[MAX_ROW][MAX_COL] = {0};
+    for(int i = 0; i < MAX_ROW; ++i)
+        for(int j = 0; j < MAX_COL; ++j)
+            if(map_image.at<cv::Vec3b>(i, j)[0] < 200)
+                safe_map[i][j] = 0;
+            else safe_map[i][j] = 1e8;
+    generate_safe_map(safe_map);
+    cv::imshow("PathPlanner Test", map_image);
+    while (true) {
+        while (operationStatus != Planning) cv::waitKey(30);
+
+        planner->setBackwardEnabled(backward_enabled);
+        planner->setCurrentSpeed(current_speed);
+        planner->setStartMaintainedPath(vector<TiEV::Pose>());
+        planner->setAbsSafeMap(safe_map);
+        planner->setLaneSafeMap(safe_map);
+        planner->setTargets(vector<TiEV::Pose>(1, targetPose));
         planner->plan();
 
-        cout << getTimeStamp() - start_time << "us" << endl;
-
-        plannerView->draw(view);
-
-        cv::imshow("PathPlanner Test", view);
-        while(cv::waitKey(100)!=27);
-
-#ifdef DEBUG
-        int current_index = -1;
-        char a;
-        cout << "[E]: Exit point viewer, [N]: Next point, [P] Previous point" << endl;
-        while((a = getch()) != 'E' && a != 'e'){
-            if(current_index >= 0)
-                setColor(view, round(res[0][current_index].x),
-                    round(res[0][current_index].y),
-                    255, 255, 255);
-            if(a == 'n' || a == 'N') ++current_index;
-            else if(a == 'p' || a == 'P') --current_index;
-            else continue;
-
-            while(current_index >= res[0].size()) current_index -= res[0].size();
-            while(current_index < 0) current_index += res[0].size();
-
-            setColor(view, round(res[0][current_index].x),
-                round(res[0][current_index].y),
-                255, 0, 0);
-
-            cout << "\r                                                                   \rx:" << res[0][current_index].x << " y:"
-                << res[0][current_index].y << " angle:"
-                << res[0][current_index].angle.getRad() << " heuristic:"
-                << res[0][current_index].s << " cost:"
-                << res[0][current_index].v;
-
-            cv::imshow("PathPlanner Test", view);
-            waitKey(1);
-        }
-#endif
-
-        cout << endl << "Target number: ";
+        cv::Mat view_image = map_image.clone();
+        draw(planner, view_image);
+        cv::imshow("PathPlanner Test", view_image);
+        operationStatus = WaitingForTargetPoint;
     }
-
     return 0;
 }
-
-#else
-int main(){
-	cout << "NO_SPEED_PLANNER must be defined to test path_planner independently" << endl;
-	return 0;
-}
-#endif
