f2d2fa4bfd78cc1ab980e1118ed6cb218f45af94
fuzhiqiang <fu0zhiqiang@163.com>
2022-01-07 16:22:52 +0800

    [TrafficLight] add traffic light detection

---
+++ src/modules/vision/vision_traffic_detect/Zcm_transceiver.py
@@ -0,0 +1,18 @@
+import time
+from zerocm import ZCM
+from MsgTrafficLightSignal import MsgTrafficLightSignal
+
+class ZcmTransceiver(object):
+    def __init__(self):
+        self.transceiver = ZCM('ipc')
+        if not self.transceiver.good():
+            print("Unable to initialize ZeroCM")
+            exit(-1)
+
+    def __call__(self, left: bool = True, forward: bool = True, right: bool = True):
+        msg_traffic_light_signal = MsgTrafficLightSignal()
+        msg_traffic_light_signal.timestamp = int(time.time())
+        msg_traffic_light_signal.left = left
+        msg_traffic_light_signal.forward = forward
+        msg_traffic_light_signal.right = right
+        self.transceiver.publish("msgTrafficLightSignal", msg_traffic_light_signal)
---
+++ src/modules/vision/vision_traffic_detect/start.sh
@@ -0,0 +1,3 @@
+#python3 detect.py --source basler --weights best2.pt --conf 0.25
+python3 detect.py --weights best2.pt --conf 0.25
+# utils/datasets.py +328 set ip
