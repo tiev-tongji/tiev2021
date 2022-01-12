b4165f5743f1ccb22e992a94df3d6ae66e98d1b7
huangchang <m18916085239@163.com>
2022-01-12 17:11:27 +0800

    [Planner] modify run_all.sh

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -15,6 +15,9 @@
 #10:
 ##################################
 
+#configure tmux
+echo "set-option -g mouse on" | tee ~/.tmux.conf
+
 #check this ubuntu whether has tmux
 type tmux
 if [ $? != 0 ]
@@ -27,23 +30,28 @@ fi
 tmux has-session -t tiev
 if [ $? != 0 ]
 then
-	tmux new-session -s tiev -n tiev2021 -d
+	tmux new -s tiev -n tiev2021 -d
 	tmux split-window -v -t tiev:tiev2021.0
-	tmux split-window -h -t tiev:tiev2021.1
-	tmux split-window -h -t tiev:tiev2021.2
-	tmux split-window -h -t tiev:tiev2021.3
+	tmux split-window -v -t tiev:tiev2021.1
+	tmux split-window -v -t tiev:tiev2021.2
+	#tmux split-window -h -t tiev:tiev2021.3
 	#start visualization
-	tmux send-keys -t tiev:tiev2021.0 'cd ~/tiev2021/scripts/' C-m
-	tmux send-keys -t tiev:tiev2021.0 './visualization.sh' C-m
+	tmux send-keys -t tiev:tiev2021.0 'cd ~/tiev/scripts/' C-m
+	tmux send-keys -t tiev:tiev2021.0 './planner.sh' C-m
 	#start planner
-	tmux send-keys -t tiev:tiev2021.1 'cd ~/tiev2021/scripts/' C-m
-	tmux send-keys -t tiev:tiev2021.1 './planner.sh' C-m
+	tmux send-keys -t tiev:tiev2021.1 'cd ~/tiev/scripts/' C-m
+	tmux send-keys -t tiev:tiev2021.1 './trajectory_controller.sh' C-m
 	#start trajectory controller
-	tmux send-keys -t tiev:tiev2021.2 'cd ~/tiev2021/scripts/' C-m
-	tmux send-keys -t tiev:tiev2021.2 './trajectory_controller.sh' C-m
+	tmux send-keys -t tiev:tiev2021.2 'cd ~/tiev/scripts/' C-m
+	#start trajectory controller
+	tmux send-keys -t tiev:tiev2021.3 'cd ~/tiev/src/cfg/' C-m
+	tmux send-keys -t tiev:tiev2021.3 'tmux select-pane -U' C-m
+	#start can control ZLG
+	#tmux send-keys -t tiev:tiev2021.3 'cd ~/tiev/scripts/' C-m
+	#tmux send-keys -t tiev:tiev2021.3 './can_control.sh' C-m
 	#start ControlCenterZLG_sim
-	tmux send-keys -t tiev:tiev2021.3 'cd ~/tiev2021/scripts/' C-m
-	tmux send-keys -t tiev:tiev2021.3 './controlcenterzlg_sim.sh' C-m
+	#tmux send-keys -t tiev:tiev2021.3 'cd ~/tiev/scripts/' C-m
+	#tmux send-keys -t tiev:tiev2021.3 './controlcenterzlg_sim.sh' C-m
 	##start vehicle controller
 	#tmux send-keys -t tiev:tiev2021.3 'cd ~/tiev2021/src/modules/planner/build/src' C-m
 	#tmux send-keys -t tiev:tiev2021.3 './planner' C-m
